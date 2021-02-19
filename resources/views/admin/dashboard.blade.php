3aa9f9dcd9add0d5e3311632f390d508b9c29486
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-19 14:50:55 +0700

    fix logo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -52,7 +52,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                        <i class="far fa-file"></i>
+                        <i class="far fa-bookmark"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
@@ -67,7 +67,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-success">
-                        <i class="fas fa-circle"></i>
+                        <i class="fas fa-fire"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
